56503447b00925cb2772319ecd99cfcdd1ecf887
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-24 13:43:28 +0100

    add new files

--- src/main/java/net/yusuf/core/RealYusufIsmailCore.java
+++ src/main/java/net/yusuf/core/RealYusufIsmailCore.java
@@ -1,12 +1,44 @@
 package net.yusuf.core;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Mod("realyusufismailcore")
 @Mod.EventBusSubscriber(modid = RealYusufIsmailCore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RealYusufIsmailCore {
     public static final Logger LOGGER = LogManager.getLogger();
+    public static final Random RANDOM = new Random();
     public static final String MOD_ID = "realyusufismailcore";
+    private static RealYusufIsmailCore INSTANCE;
+    public static SideProxy PROXY;
+
+    public RealYusufIsmailCore() {
+        INSTANCE = this;
+        PROXY = DistExecutor.safeRunForDist(() -> SideProxy.Client::new, () -> SideProxy.Server::new);
+    }
+    public static String getVersion() {
+        Optional<? extends ModContainer> o = ModList.get().getModContainerById(MOD_ID);
+        if (o.isPresent()) {
+            return o.get().getModInfo().getVersion().toString();
+        }
+        return "0.0.0";
+    }
+
+    public static boolean isDevBuild() {
+        return "NONE".equals(getVersion());
+    }
+
+    public static ResourceLocation getId(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
 }
---
+++ src/main/java/net/yusuf/core/SideProxy.java
@@ -0,0 +1,75 @@
+package net.yusuf.core;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.impl.TeleportCommand;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.tags.TagRegistryManager;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.event.lifecycle.*;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+public class SideProxy {
+
+    SideProxy() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::gatherData);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::imcEnqueue);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::imcProcess);
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(IRecipeSerializer.class, this::registerRecipeSerializers);
+
+        MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
+
+    }
+    private void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        gen.addProvider(new TestRecipeProvider(gen));
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {}
+
+    private void imcEnqueue(InterModEnqueueEvent event) {}
+
+    private void imcProcess(InterModProcessEvent event) {}
+
+    private void onRegisterCommands(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSource> dispatcher = event.getDispatcher();
+        DisplayNBTCommand.register(dispatcher);
+        TeleportCommand.register(dispatcher);
+    }
+
+    private void registerRecipeSerializers(RegistryEvent.Register<IRecipeSerializer<?>> event) {
+        event.getRegistry().register(DamageItemRecipe.SERIALIZER.setRegistryName(SilentLib.getId("damage_item")));
+    }
+
+    private void registerIngredientSerializers() {
+        CraftingHelper.register(ExclusionIngredient.Serializer.NAME, ExclusionIngredient.Serializer.INSTANCE);
+    }
+
+    public void tryFetchTagsHack() {}
+
+    static class Client extends SideProxy {
+        Client() {
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+        }
+
+        private void clientSetup(FMLClientSetupEvent event) {}
+
+        @Override
+        public void tryFetchTagsHack() {
+            TagRegistryManager.fetchTags();
+        }
+    }
+
+    static class Server extends SideProxy {
+        Server() {
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverSetup);
+        }
+
+        private void serverSetup(FMLDedicatedServerSetupEvent event) {}
+    }
+}
