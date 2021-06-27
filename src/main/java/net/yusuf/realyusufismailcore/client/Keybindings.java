05a35c6f55307ad53536048baaff03daad5e30ca
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-27 22:05:31 +0100

    update

--- src/main/java/net/yusuf/realyusufismailcore/client/Keybindings.java
+++ src/main/java/net/yusuf/realyusufismailcore/client/Keybindings.java
@@ -1,8 +1,10 @@
 package net.yusuf.realyusufismailcore.client;
 
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.NonNullList;
 
 public class Keybindings {
+    public static NonNullList<KeyBinding> keyBinds = NonNullList.create();
 
     public static KeyBinding RELOAD;
 }
