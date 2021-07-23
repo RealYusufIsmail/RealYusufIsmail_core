6568c2366a53bd656b2e655d8824c4b6e77e4683
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-23 12:54:20 +0100

    Update ItemInitCore.java

--- src/main/java/net/yusuf/realyusufismailcore/core/init/ItemInitCore.java
+++ src/main/java/net/yusuf/realyusufismailcore/core/init/ItemInitCore.java
@@ -1,7 +1,8 @@
 package net.yusuf.realyusufismailcore.core.init;
 
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.RegistryObject;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.yusuf.realyusufismailcore.RealYusufIsmailCore;
