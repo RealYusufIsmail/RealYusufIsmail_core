0a08b51ce04ceb1b43a18330e7bae6652cee6724
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-23 16:46:10 +0100

    Update OutputSlot.java

--- src/main/java/net/yusuf/realyusufismailcore/util/OutputSlot.java
+++ src/main/java/net/yusuf/realyusufismailcore/util/OutputSlot.java
@@ -1,17 +1,18 @@
 package net.yusuf.realyusufismailcore.util;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.container.Slot;
-import net.minecraft.item.ItemStack;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
 
 public class OutputSlot extends Slot {
-    public OutputSlot(IInventory p_i1824_1_, int p_i1824_2_, int p_i1824_3_, int p_i1824_4_) {
+    public OutputSlot(Inventory p_i1824_1_, int p_i1824_2_, int p_i1824_3_, int p_i1824_4_) {
         super(p_i1824_1_, p_i1824_2_, p_i1824_3_, p_i1824_4_);
     }
 
     @Override
-    public boolean mayPickup(PlayerEntity p_82869_1_) {
+    public boolean mayPickup(Player p_82869_1_) {
         return true;
     }
 
