99638de915c9028c924759624d26b5cbcb7356e8
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-02-22 20:36:38 +0000

    1.19-1.0.8

---
+++ src/main/java/io/github/realyusufismail/realyusufismailcore/data/support/oregen/ModOreFeaturesSupport.java
@@ -0,0 +1,49 @@
+package io.github.realyusufismail.realyusufismailcore.data.support.oregen;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+
+import java.util.List;
+
+public abstract class ModOreFeaturesSupport {
+    protected RuleTest ruleTest1 = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+    protected RuleTest ruleTest2 = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+    protected RuleTest ruleTest3 = new BlockMatchTest(Blocks.NETHERRACK);
+    protected RuleTest ruleTest4 = new TagMatchTest(BlockTags.BASE_STONE_NETHER);
+
+
+    public abstract void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context);
+
+    private static void registerOre(BootstapContext<ConfiguredFeature<?, ?>> context,
+            ResourceKey<ConfiguredFeature<?, ?>> ore,
+            List<OreConfiguration.TargetBlockState> targetBlockStates, int size) {
+        context.register(ore, new ConfiguredFeature<>(Feature.ORE,
+                new OreConfiguration(targetBlockStates, size)));
+    }
+
+    private static void registerOre(BootstapContext<ConfiguredFeature<?, ?>> context,
+            ResourceKey<ConfiguredFeature<?, ?>> ore, RuleTest ruleTest, BlockState blockState,
+            int size) {
+        context.register(ore, new ConfiguredFeature<>(Feature.ORE,
+                new OreConfiguration(ruleTest, blockState, size)));
+    }
+
+    protected static ResourceKey<ConfiguredFeature<?, ?>> createKey(String name, String modId) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE,
+                new ResourceLocation(modId, name.toLowerCase()));
+    }
+}
---
+++ src/main/java/io/github/realyusufismail/realyusufismailcore/data/support/oregen/ModOrePlacementsSupport.java
@@ -0,0 +1,45 @@
+package io.github.realyusufismail.realyusufismailcore.data.support.oregen;
+
+import net.minecraft.core.Holder;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.*;
+import net.minecraft.core.registries.Registries;
+
+import java.util.List;
+
+public abstract class ModOrePlacementsSupport {
+
+    // example :
+    // public static final ResourceKey<PlacedFeature> LEGENDARY_ORE = createKey("legendary_ore");
+
+    public abstract void bootstrap(BootstapContext<PlacedFeature> context);
+
+    private static void register(BootstapContext<PlacedFeature> context,
+            ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> holder,
+            List<PlacementModifier> placement) {
+        context.register(key, new PlacedFeature(holder, placement));
+    }
+
+    protected static List<PlacementModifier> orePlacement(PlacementModifier modifier,
+            PlacementModifier modifier1) {
+        return List.of(modifier, InSquarePlacement.spread(), modifier1, BiomeFilter.biome());
+    }
+
+    protected static List<PlacementModifier> commonOrePlacement(int pCount,
+            PlacementModifier pHeightRange) {
+        return orePlacement(CountPlacement.of(pCount), pHeightRange);
+    }
+
+    protected static List<PlacementModifier> rareOrePlacement(int pChance,
+            PlacementModifier pHeightRange) {
+        return orePlacement(RarityFilter.onAverageOnceEvery(pChance), pHeightRange);
+    }
+
+    protected static ResourceKey<PlacedFeature> createKey(String name, String modId) {
+        return ResourceKey.create(Registries.PLACED_FEATURE,
+                new ResourceLocation(modId, name.toLowerCase()));
+    }
+}
